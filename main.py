60e9cb63d96ec922a5c64d891225c3505d98c7c9
deeonnnn <deon.tan02@gmail.com>
2022-07-27 18:00:52 +0800

    initilize commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import api, cash_on_hand, overheads, profit_loss
+
+
+
+print("Hello World! This is our Team's solution!")
+
+def add():
+    print("This is function add")
+    return
+
+def delete():
+    print("This is function delete")
+    return
+
+def multiply():
+    print("This is function multiply")
+    return
+add()
+delete()
+multiply()
