5f2dc4312ba181db306895d5d087cfa5f299ce2b
Ng Yi Kai <ngyikai05@gmail.com>
2022-07-29 11:37:33 +0800

    initialise

--- read_file.py
+++ read_file.py
@@ -2,14 +2,15 @@ from pathlib import Path
 import csv
 from pickle import TRUE
 filepath1=Path.cwd()/"csv_reports"/"Cash On Hand.csv"
-print(filepath1.exists())
-def process_csv_file():
+# print(filepath1.exists())
+def processcoh():
     #cashonhand
     with filepath1.open(mode="r", encoding="UTF-8", newline="")as file:
         reader=csv.reader(file)
         next(reader)
         day=0
         coh=0
+        anotherlist=[]
         for row in reader:
             if day==0:
                 day=int(row[0])
@@ -18,11 +19,44 @@ def process_csv_file():
                 if coh>int(row[1]):
                     cohdiff=int(row[1])-coh
                     day=int(row[0])
-                    print(f"{day-1}-{day}")
-                    print(cohdiff)
+                    anotherlist.append(f"[CASH DEFICIT] US${cohdiff} on day {day}")
+        return anotherlist
 
+print(processcoh())
 
+filepath2=Path.cwd()/"csv_reports"/"Overheads.csv"
+# print(filepath2.exists())
+def processovrhds():
+    #cashonhand
+    with filepath2.open(mode="r", encoding="UTF-8", newline="")as file:
+        reader=csv.reader(file)
+        next(reader)
+        # for row in reader:
+        #     print(row)
+        expense=0
+        for row in reader:
+            if expense<float(row[1]):
+                expense=float(row[1])
+                return(f"[HIGHEST OVERHEADS] {row[0]}: US${row[1]}")
 
+print(processovrhds())
 
+filepath3=Path.cwd()/"csv_reports"/"Profit and Loss.csv"
+def processproflos():
+    with filepath3.open(mode="r", encoding="UTF-8", newline="")as file:
+        reader=csv.reader(file)
+        next(reader)
+        day=0
+        prof=0
+        for row in reader:
+            # print(row)
+            if day==0:
+                day=int(row[0])
+                prof=int(row[4])
+            elif day<int(row[0]):
+                if prof>int(row[4]):
+                    profdiff=int(row[4])-prof
+                    day=int(row[0])
+                    return(f" [PROFIT DEFICIT]US${profdiff} on day {day}")
 
-print(process_csv_file())
+print(processproflos())
