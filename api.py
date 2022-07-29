fb533570532a19f89cfe67277ec4833199936cf6
Ng Yi Kai <ngyikai05@gmail.com>
2022-07-29 15:18:05 +0800

    ini

--- api.py
+++ api.py
@@ -2,19 +2,21 @@ import requests
 from pathlib import Path
 import csv
 import json
+def apifunction():
+    home = Path.home()
+    print(home)
 
-home = Path.home()
-print(home)
-
-url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=FWCQ6BR9LLASNVQR"
-function= "CURRENCY_EXCHANGE_RATE"
-from_currency = "USD"
-to_currency ="SGD"
-response = requests.get(url)
-print(response)
-data = response.json()
-print(data)
-
+    url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=FWCQ6BR9LLASNVQR"
+    function= "CURRENCY_EXCHANGE_RATE"
+    from_currency = "USD"
+    to_currency ="SGD"
+    response = requests.get(url)
+    # print(response)
+    data = response.json()
+    # print(data)
+    convrate=data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+    # print(convrate)
+    return convrate
 {
     "Realtime Currency Exchange Rate": {
         "1. From_Currency Code": "USD",
@@ -28,24 +30,3 @@ print(data)
         "9. Ask Price": "1.38861000",
     }
 }
-
-
-
-url = 'https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=FWCQ6BR9LLASNVQR'
-request = requests.get(url)
-data = request.json()
-import json
-# print(json.dumps(data["Time Series FX (Weekly)"],indent=4))
-newdata=data["Time Series FX (Weekly)"]
-empty_list = []
-for item in newdata:
-    empty_list.append(newdata[item]["4. close"])
-
-for i in range(0, len(empty_list)):
-    empty_list[i] = float(empty_list[i])
-# print(json.dumps(empty_list,indent=4))
-# print(empty_list)
-
-weekly_average=sum(empty_list) / len(empty_list)
-print(weekly_average)
-
--- write_file.py
+++ write_file.py
@@ -1,7 +1,13 @@
 from read_file import processcoh, processovrhds, processproflos
 from pathlib import Path
 textfile=Path.cwd()/"deficit_report.txt"
+# print(processovrhds())
 
 def writingfunc():
-    with textfile.open(mode="r", encoding="UTF-8", newline="")as file:
-        pass
+    actualcoh=str(processcoh())
+    actualcoh=actualcoh.strip("[""]")
+    with textfile.open(mode="w", encoding="UTF-8", newline="")as file:
+        file.write(actualcoh)
+    pass
+
+print(writingfunc())
