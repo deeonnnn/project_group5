38dd6a7cd66e18065f1a6b5553375cff937f3592
tre-esa <109326136+tre-esa@users.noreply.github.com>
2022-07-29 14:28:20 +0800

    Update write_file.py

--- write_file.py
+++ write_file.py
@@ -2,6 +2,9 @@ from read_file import processcoh, processovrhds, processproflos
 from pathlib import Path
 textfile=Path.cwd()/"deficit_report.txt"
 
+
+
+
 def writingfunc():
     with textfile.open(mode="r", encoding="UTF-8", newline="")as file:
         pass
