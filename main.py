047f14c63964deb73d644d656e3ea2fbb489c9aa
Ng Yi Kai <ngyikai05@gmail.com>
2022-08-01 11:40:42 +0800

    i

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from api import api_function
 from read_file import process_cash_on_hand, process_profit_loss, process_over_heads
-from write_file import deficit_report, deficit_report
+from write_file import deficit_report
 def main():
     deficit_report(process_over_heads(), process_cash_on_hand(), process_profit_loss(), api_function())
 
